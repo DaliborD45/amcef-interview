bc2b2151c6ed0e0b876afd6436d91ee8a1c7dcf4
DaliborD45 <dalibordetko@gmail.com>
2024-06-17 12:23:29 +0200

    created api request functions

---
+++ src/api/todoList.api.tsx
@@ -0,0 +1,34 @@
+"use server"
+
+import { TodoListT } from "@/types/todoList.type"
+import { revalidatePath } from "next/cache"
+
+export const getTodoLists = async () => {
+  try {
+    const response = await fetch(
+      "https://666c14cc49dbc5d7145c8307.mockapi.io/api/todolist"
+    )
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const addTodoList = async (todoList: TodoListT) => {
+  try {
+    const response = await fetch(
+      "https://666c14cc49dbc5d7145c8307.mockapi.io/api/todolist",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todoList),
+      }
+    )
+    revalidatePath("/")
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
